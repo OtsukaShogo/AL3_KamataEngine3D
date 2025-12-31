67f1bcc4bdb0df57f386ee23a443f6af2cc17b6d
OtsukaShogo <k025g0010@g.neec.ac.jp>
2025-12-31 14:47:56 +0900

    確認課題05_01　2Dアクション

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,14 +2,40 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {
-	
+GameScene::GameScene() {};
+
+GameScene::~GameScene() { 
+	delete model_;
+	delete player_;
+}
+
+void GameScene::Initialize() { 
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//カメラの初期化
+	camera_.Initialize();
+
+	//自キャラの生成
+	player_ = new Player();
+	//自キャラの初期化
+	player_->Initialize(model_, textureHandle_, &camera_);
 }
 
 void GameScene::Update() {
-
+	//自キャラの更新
+	player_->Update();
 }
 
-void GameScene::Draw() {
+void GameScene::Draw() { 
+	Model::PreDraw();
 
+	//自キャラの描画
+	player_->Draw();
+
+	Model::PostDraw();
 }
